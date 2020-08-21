67c2f2212bbe90f1153dbc0c811e46ffe92cabd1
Peter Opara <opara.peter@gmail.com>
2020-08-21 11:44:21 +0200

    Changed continue to pass

--- hummingbot/market/bitstamp/bitstamp_api_order_book_data_source.py
+++ hummingbot/market/bitstamp/bitstamp_api_order_book_data_source.py
@@ -222,7 +222,7 @@ class BitstampAPIOrderBookDataSource(OrderBookTrackerDataSource):
                         if msg_type is None:
                             raise ValueError(f"Bitstamp Websocket message does not contain an event type - {msg}")
                         elif msg_type == "bts:subscription_succeeded":
-                            continue
+                            pass
                         elif msg_type == "trade":
                             trade_msg: OrderBookMessage = BitstampOrderBook.trade_message_from_exchange(
                                 msg["data"],
@@ -257,7 +257,7 @@ class BitstampAPIOrderBookDataSource(OrderBookTrackerDataSource):
                         if msg_type is None:
                             raise ValueError(f"Bitstamp Websocket message does not contain an event type - {msg}")
                         elif msg_type == "bts:subscription_succeeded":
-                            continue
+                            pass
                         elif msg_type == "data":
                             order_book_message: OrderBookMessage = BitstampOrderBook.diff_message_from_exchange(
                                 msg["data"],
