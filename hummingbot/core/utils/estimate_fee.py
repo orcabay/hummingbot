5a28dfad8e1e4d6e44b7e650e1036ab028751e9c
Peter Opara <opara.peter@gmail.com>
2020-08-31 19:11:35 +0200

    Added bitstamp lines to fee estimators.

--- hummingbot/core/utils/estimate_fee.py
+++ hummingbot/core/utils/estimate_fee.py
@@ -9,6 +9,7 @@ default_cex_estimate = {
     "coinbase_pro": [0.5, 0.5],
     "huobi": [0.2, 0.2],
     "kraken": [0.16, 0.26],
+    "bitstamp": [0.5, 0.5],
     "kucoin": [0.1, 0.1],
     "liquid": [0.1, 0.1]}
 
